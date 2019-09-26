74ddfb89d06762fb0b95ea47ada2afca0febb11a
JuanACC <ing.juan.acc@gmail.com>
2019-09-25 21:12:04 -0500

    Reset

--- prueba-1-practica-cloud-inventario-rest/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into producto_model(id,nombre,port,stock)
-values(1,'Laptop',0,100);
-insert into producto_model(id,nombre,port,stock)
-values(2,'Pantalla Samsung',0,200);
-insert into producto_model(id,nombre,port,stock)
-values(3,'Xbox One X',0,300);
-insert into producto_model(id,nombre,port,stock)
-values(4,'Celular Xiaomi',0,400);
